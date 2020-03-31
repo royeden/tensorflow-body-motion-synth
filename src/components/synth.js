817c5a8bb3dc8b44d491f09f5de22aca88ca5777
RoyEden <roy.ede@gmail.com>
2020-03-31 02:49:00 -0300

    Separated even more synth logic and added frequency mapping

--- src/components/synth.js
+++ src/components/synth.js
@@ -3,20 +3,17 @@ import React, { useCallback, useContext, useMemo, useState } from "react";
 import {
   A4_440,
   BASE_TET,
-  FREQUENCY_LIMITS,
-  SYNTH_WAVE_TYPES,
-  TET_LIMITS
+  SYNTH_WAVE_TYPES
 } from "../constants/music";
 import { audioContext } from "../context/audioContext";
 import { cameraContext } from "../context/cameraContext";
-import { map, mapWithinBoundary } from "../utils/math";
 import useOscillator from "../hooks/useOscillator";
 import useTemperamentScale from "../hooks/useTemperamentScale";
 import useToggle from "../hooks/useToggle";
 
 import Input from "./input";
 import Select from "./select";
-import SynthBodyControls from "./synthBodyControls";
+import SynthFrequencyControls from "./synthFrequencyControls";
 
 function Synth({ id, person, personId, removeSynth }) {
   const { audioContextObject } = useContext(audioContext);
@@ -25,9 +22,7 @@ function Synth({ id, person, personId, removeSynth }) {
   } = useContext(cameraContext);
   const [canPlay, setCanPlay] = useState(false);
 
-  const [minNote, setMinNote] = useState(0);
   const [centerNote, setCenterNote] = useState(A4_440.position);
-  const [maxNote, setMaxNote] = useState(88);
   const [baseFrequency, setBaseFrequency] = useState(A4_440.frequency);
   const [synthWaveType, setSynthWaveType] = useState(SYNTH_WAVE_TYPES[0]);
   const [tet, setTet] = useState(BASE_TET);
@@ -40,29 +35,9 @@ function Synth({ id, person, personId, removeSynth }) {
     }
   );
   const [muted, toggleMuted] = useToggle(true);
+  const [notes, toggleNotes] = useToggle(true);
   const [persist, togglePersist] = useToggle(true);
   const [resetSynthOnUpdate, toggleResetSynthOnUpdate] = useToggle(true);
-
-  const maxNoteValidation = useCallback(
-    value => value >= minNote && value <= 166,
-    [minNote]
-  );
-  const centerNoteValidation = useCallback(
-    value => value >= minNote && value <= maxNote,
-    [maxNote, minNote]
-  );
-  const minNoteValidation = useCallback(
-    value => value && value >= 0 && value <= maxNote,
-    [maxNote]
-  );
-  const frequencyValidation = useCallback(
-    value => value >= FREQUENCY_LIMITS.min && value <= FREQUENCY_LIMITS.max,
-    []
-  );
-  const tetValidation = useCallback(
-    value => value >= TET_LIMITS.min && value <= TET_LIMITS.max,
-    []
-  );
 
   const handleRemove = useCallback(() => removeSynth(id), [id, removeSynth]);
 
@@ -84,55 +59,6 @@ function Synth({ id, person, personId, removeSynth }) {
     [id, personId]
   );
 
-  const handlePositionChange = useCallback(
-    position => {
-      const x = position.x && mapWithinBoundary(position.x, 0, width);
-      const y = position.y && mapWithinBoundary(position.y, 0, height);
-      const mappedX = x
-        ? map(x, 0, width, minNote, y ? maxNote / 2 : maxNote)
-        : 0;
-      const mappedY = y
-        ? map(y, 0, height, minNote, x ? maxNote / 2 : maxNote)
-        : 0;
-      const note = Math.round(mappedX + mappedY);
-      getNote(note);
-    },
-    [getNote, height, maxNote, minNote, width]
-  );
-
-  const handleMinNoteChange = useCallback(
-    value => {
-      const parsedValue = parseFloat(value, 10);
-      setMinNote(parsedValue);
-      if (resetSynthOnUpdate) setFrequency(baseFrequency);
-    },
-    [baseFrequency, resetSynthOnUpdate, setFrequency]
-  );
-
-  const handleMaxNoteChange = useCallback(
-    value => {
-      const parsedValue = parseFloat(value, 10);
-      setMaxNote(parsedValue);
-      if (resetSynthOnUpdate) setFrequency(baseFrequency);
-    },
-    [baseFrequency, resetSynthOnUpdate, setFrequency]
-  );
-  const handleCenterNoteChange = useCallback(
-    value => {
-      const parsedValue = parseFloat(value, 10);
-      setCenterNote(parsedValue);
-      if (resetSynthOnUpdate) setFrequency(baseFrequency);
-    },
-    [baseFrequency, resetSynthOnUpdate, setFrequency]
-  );
-  const handlBaseFrequencyChange = useCallback(value => {
-    const parsedValue = parseFloat(value, 10);
-    setBaseFrequency(parsedValue);
-    if (resetSynthOnUpdate) setFrequency(parsedValue);
-  }, [resetSynthOnUpdate, setFrequency]);
-
-  const handlTetChange = useCallback(value => setTet(parseInt(value, 10)), []);
-
   return (
     <div>
       <h1>
@@ -145,53 +71,24 @@ function Synth({ id, person, personId, removeSynth }) {
         onChange={setSynthWaveType}
         options={synthOptions}
       />
-      <SynthBodyControls
+      <SynthFrequencyControls
+        baseFrequency={baseFrequency}
         canPlay={canPlay}
+        centerNote={centerNote}
+        getNote={getNote}
+        height={height}
         id={id}
-        onPositionChange={handlePositionChange}
+        notes={notes}
         person={person}
         personId={personId}
+        resetSynthOnUpdate={resetSynthOnUpdate}
+        setBaseFrequency={setBaseFrequency}
         setCanPlay={setCanPlay}
-      />
-      <Input
-        defaultValue={minNote}
-        label="Min note:"
-        labelIdPrefix={`Synth_${personId}_${id}_min_note`}
-        min={0}
-        max={166}
-        onChange={handleMinNoteChange}
-        type="number"
-        validation={minNoteValidation}
-      />
-      <Input
-        defaultValue={centerNote}
-        label="Center note:"
-        labelIdPrefix={`Synth_${personId}_${id}_center_note`}
-        min={0}
-        max={166}
-        onChange={handleCenterNoteChange}
-        type="number"
-        validation={centerNoteValidation}
-      />
-      <Input
-        defaultValue={maxNote}
-        label="Max note:"
-        labelIdPrefix={`Synth_${personId}_${id}_max_note`}
-        min={0}
-        max={166}
-        onChange={handleMaxNoteChange}
-        type="number"
-        validation={maxNoteValidation}
-      />
-      <Input
-        defaultValue={baseFrequency}
-        label="Base Frequency"
-        labelIdPrefix={`Synth_${personId}_${id}_frequency`}
-        max={FREQUENCY_LIMITS.max}
-        min={FREQUENCY_LIMITS.min}
-        onChange={handlBaseFrequencyChange}
-        type="number"
-        validation={frequencyValidation}
+        setCenterNote={setCenterNote}
+        setFrequency={setFrequency}
+        setTet={setTet}
+        tet={tet}
+        width={width}
       />
       <Input
         checked={resetSynthOnUpdate}
@@ -202,12 +99,12 @@ function Synth({ id, person, personId, removeSynth }) {
         type="checkbox"
       />
       <Input
-        defaultValue={tet}
-        label="Tet:"
-        labelIdPrefix={`Synth_${personId}_${id}_tet`}
-        onChange={handlTetChange}
-        type="number"
-        validation={tetValidation}
+        checked={notes}
+        defaultValue={notes}
+        label="Use chromatic notes:"
+        labelIdPrefix={`Synth_${personId}_${id}_use_notes`}
+        onChange={toggleNotes}
+        type="checkbox"
       />
       <Input
         checked={persist}
---
+++ src/components/synthFrequencyControls.js
@@ -0,0 +1,206 @@
+import React, { useCallback, useState } from "react";
+
+import Input from "./input";
+import { FREQUENCY_LIMITS, TET_LIMITS } from "../constants/music";
+import { map, mapWithinBoundary } from "../utils/math";
+import SynthBodyControls from "./synthBodyControls";
+
+function SynthFrequencyControls({
+  baseFrequency,
+  canPlay,
+  centerNote,
+  getNote,
+  height,
+  id,
+  notes,
+  person,
+  personId,
+  resetSynthOnUpdate,
+  setBaseFrequency,
+  setCanPlay,
+  setCenterNote,
+  setFrequency,
+  setTet,
+  tet,
+  width
+}) {
+  const [minNote, setMinNote] = useState(0);
+  const [maxNote, setMaxNote] = useState(88);
+
+  const maxNoteValidation = useCallback(
+    value => value >= minNote && value <= 166,
+    [minNote]
+  );
+  const centerNoteValidation = useCallback(
+    value => value >= minNote && value <= maxNote,
+    [maxNote, minNote]
+  );
+  const minNoteValidation = useCallback(
+    value => value && value >= 0 && value <= maxNote,
+    [maxNote]
+  );
+
+  const frequencyValidation = useCallback(
+    value => value >= FREQUENCY_LIMITS.min && value <= FREQUENCY_LIMITS.max,
+    []
+  );
+  const tetValidation = useCallback(
+    value => value >= TET_LIMITS.min && value <= TET_LIMITS.max,
+    []
+  );
+
+  const handleFrequencyPositionChange = useCallback(
+    position => {
+      const x = position.x && mapWithinBoundary(position.x, 0, width);
+      const y = position.y && mapWithinBoundary(position.y, 0, height);
+      const mappedX = x
+        ? map(
+            x,
+            0,
+            width,
+            FREQUENCY_LIMITS.min,
+            y ? FREQUENCY_LIMITS.max / 2 : FREQUENCY_LIMITS.max
+          )
+        : 0;
+      const mappedY = y
+        ? map(
+            y,
+            0,
+            height,
+            FREQUENCY_LIMITS.min,
+            x ? FREQUENCY_LIMITS.max / 2 : FREQUENCY_LIMITS.max
+          )
+        : 0;
+      setFrequency(
+        mappedX > 0 || mappedY > 0
+          ? // TODO figure out how to scale accordingly, remeber that frequency is a logarythmic scale
+            (mappedX + mappedY) * (1 / Math.log(mappedX + mappedY))
+          : FREQUENCY_LIMITS.min
+      );
+    },
+    [height, setFrequency, width]
+  );
+
+  const handleNotePositionChange = useCallback(
+    position => {
+      const x = position.x && mapWithinBoundary(position.x, 0, width);
+      const y = position.y && mapWithinBoundary(position.y, 0, height);
+      const mappedX = x
+        ? map(x, 0, width, minNote, y ? maxNote / 2 : maxNote)
+        : 0;
+      const mappedY = y
+        ? map(y, 0, height, minNote, x ? maxNote / 2 : maxNote)
+        : 0;
+      const note = Math.round(mappedX + mappedY);
+      getNote(note);
+    },
+    [getNote, height, maxNote, minNote, width]
+  );
+
+  const handleMinNoteChange = useCallback(
+    value => {
+      const parsedValue = parseFloat(value, 10);
+      setMinNote(parsedValue);
+      if (resetSynthOnUpdate) setFrequency(baseFrequency);
+    },
+    [baseFrequency, resetSynthOnUpdate, setFrequency]
+  );
+
+  const handleMaxNoteChange = useCallback(
+    value => {
+      const parsedValue = parseFloat(value, 10);
+      setMaxNote(parsedValue);
+      if (resetSynthOnUpdate) setFrequency(baseFrequency);
+    },
+    [baseFrequency, resetSynthOnUpdate, setFrequency]
+  );
+  const handleCenterNoteChange = useCallback(
+    value => {
+      const parsedValue = parseFloat(value, 10);
+      setCenterNote(parsedValue);
+      if (resetSynthOnUpdate) setFrequency(baseFrequency);
+    },
+    [baseFrequency, resetSynthOnUpdate, setCenterNote, setFrequency]
+  );
+  const handlBaseFrequencyChange = useCallback(
+    value => {
+      const parsedValue = parseFloat(value, 10);
+      setBaseFrequency(parsedValue);
+      if (resetSynthOnUpdate) setFrequency(parsedValue);
+    },
+    [resetSynthOnUpdate, setBaseFrequency, setFrequency]
+  );
+
+  const handlTetChange = useCallback(value => setTet(parseInt(value, 10)), [
+    setTet
+  ]);
+
+  return (
+    <>
+      <SynthBodyControls
+        canPlay={canPlay}
+        id={id}
+        onPositionChange={
+          notes ? handleNotePositionChange : handleFrequencyPositionChange
+        }
+        person={person}
+        personId={personId}
+        setCanPlay={setCanPlay}
+      />
+      {notes && (
+        <>
+          <Input
+            defaultValue={minNote}
+            label="Min note:"
+            labelIdPrefix={`Synth_${personId}_${id}_min_note`}
+            min={0}
+            max={166}
+            onChange={handleMinNoteChange}
+            type="number"
+            validation={minNoteValidation}
+          />
+          <Input
+            defaultValue={centerNote}
+            label="Center note:"
+            labelIdPrefix={`Synth_${personId}_${id}_center_note`}
+            min={0}
+            max={166}
+            onChange={handleCenterNoteChange}
+            type="number"
+            validation={centerNoteValidation}
+          />
+          <Input
+            defaultValue={maxNote}
+            label="Max note:"
+            labelIdPrefix={`Synth_${personId}_${id}_max_note`}
+            min={0}
+            max={166}
+            onChange={handleMaxNoteChange}
+            type="number"
+            validation={maxNoteValidation}
+          />
+          <Input
+            defaultValue={tet}
+            label="Tet:"
+            labelIdPrefix={`Synth_${personId}_${id}_tet`}
+            onChange={handlTetChange}
+            type="number"
+            validation={tetValidation}
+          />
+          <Input
+            defaultValue={baseFrequency}
+            label="Base Frequency"
+            labelIdPrefix={`Synth_${personId}_${id}_frequency`}
+            max={FREQUENCY_LIMITS.max}
+            min={FREQUENCY_LIMITS.min}
+            onChange={handlBaseFrequencyChange}
+            type="number"
+            validation={frequencyValidation}
+          />
+        </>
+      )}
+    </>
+  );
+}
+
+export default SynthFrequencyControls;
